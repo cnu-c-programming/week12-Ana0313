db39d77a9c3eb2cc19c3d2605e67c00df8b42b36
Ana0313 <rokafosh2@gmail.com>
2026-05-28 23:06:02 +0900

    Update 2-6.c

--- code/2-6.c
+++ code/2-6.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, const char* argv[]) {
-    if (argc < 3)
+int main(int argc, const char* argv[]){
+    if(argc<3)
         return 0;
 
-    FILE* fp = fopen(argv[1], "r");
-    const char* target_str = argv[2];
+    FILE* fp=fopen(argv[1],"r");
+    const char* target_str=argv[2];
 
+    if(fp==NULL)
+        return 0;
 
-    
+    char bf[256]={0};
+    while(fgets(bf,sizeof(bf),fp)!=NULL){
+        if(strstr(bf,target_str)!=NULL){
+            printf("%s",bf);
+        }
+    }
+
     fclose(fp);
+    return 0;
 }
-
